d930906486b2b2ecf461bca9d7d931870c5eb1b6
AlyyMalick <alyymalick@projjexx.co>
2023-10-17 19:21:06 +0500

    hello

---
+++ jsm_portfolio/backend_sanity/schemas/index.js
@@ -0,0 +1,29 @@
+// import testimonials from './testimonials'
+// export const schemaTypes = [
+//     types: schemaTypes.concat([
+//         testimonials
+//     ]),
+// ]
+
+import testimonials from './testimonials'
+import works from './works'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+export const schemaTypes = [
+  // Your other schema types here
+  testimonials,
+  works,
+  brands,
+  abouts,
+  experiences,
+  skills,
+  workExperience,
+  contact,
+]
+
+export default schemaTypes
