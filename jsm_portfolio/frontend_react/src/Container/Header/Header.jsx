7559a5745d1ab06927d5df3043ce2b4ec856702f
H-Chishtee <m.hasanzubair01@gmail.com>
2023-10-19 21:09:46 +0500

    Added icons & slider

--- jsm_portfolio/frontend_react/src/Container/Header/Header.jsx
+++ jsm_portfolio/frontend_react/src/Container/Header/Header.jsx
@@ -6,7 +6,6 @@
 
 // export default Header;
 
-
 // import React from "react";
 // import { images } from "../../Constants";
 // import "./Header.scss";
@@ -23,7 +22,7 @@
 //         <p className="hero-subheading">
 //           Boost productivity and efficiency with our innovative software solutions. Let us take your business to the next level.
 //         </p>
-//         <img src={images.profile} alt="profile"> 
+//         <img src={images.profile} alt="profile">
 //       </div>
 //     </div>
 //   );
@@ -31,27 +30,25 @@
 
 // export default Header;
 
-
-
-
 // Header.jsx
 import React from "react";
 import "./Header.scss";
-
+import { AppWrap } from "../../wrapper";
 const Header = () => {
   return (
     <header className="app__header">
       <div className="app__header-content">
-        <h1>Transform Your Business <br />
-            With Cutting-Edge
-            Software</h1>
-        <h3>boost productivity and efficiency with our
-             innovative <br />  software solutions. Let us
-              take  your <br /> business to the next level</h3>
+        <h1>
+          Transform Your Business <br />
+          With Cutting-Edge Software
+        </h1>
+        <h3>
+          boost productivity and efficiency with our innovative <br /> software
+          solutions. Let us take your <br /> business to the next level
+        </h3>
       </div>
     </header>
   );
 };
 
-export default Header;
-
+export default AppWrap(Header, "home");
--- jsm_portfolio/frontend_react/src/client.js
+++ jsm_portfolio/frontend_react/src/client.js
@@ -1,12 +1,11 @@
-import snaityClient, { SanityClient } from "@sanity/client";
+import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
-export const client = new SanityClient({
-  //   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-  projectId: "lms0ouhc",
+export const client = sanityClient({
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2023-10-19",
-  seCdn: true,
+  useCdn: true,
   token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
