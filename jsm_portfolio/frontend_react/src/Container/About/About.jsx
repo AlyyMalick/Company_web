6e5c5a0fbf96dbebd2f13afb9a35586c28af70bf
H-Chishtee <m.hasanzubair01@gmail.com>
2023-10-20 10:56:46 +0500

    testimonial created

--- jsm_portfolio/frontend_react/src/Container/About/About.jsx
+++ jsm_portfolio/frontend_react/src/Container/About/About.jsx
@@ -3,46 +3,42 @@ import { motion } from "framer-motion";
 // import React from "react";
 import { images } from "../../Constants";
 import { AppWrap } from "../../wrapper";
-import { urlFor, client } from '../../client';
-
-
+import { urlFor, client } from "../../client";
 
 import "./About.scss";
 
-// const abouts = [
-//   {
-//     title: "Web Development",
-//     description: "We have the best web developers.",
-//     imgUrl: images.about01,
-//   },
-//   {
-//     title: "React Native Developer",
-//     description:
-//       "We have the best React Native developers with a passion for building beautiful mobile applications.",
-//     imgUrl: images.about02,
-//   },
-//   {
-//     title: "UI/UX",
-//     description: "Professionals in User Interface and User Experience.",
-//     imgUrl: images.about03,
-//   },
-//   {
-//     title: "Data Engineering",
-//     description: "We have the best Data Engineers.",
-//     imgUrl: images.about04,
-//   },
-// ];
+const abouts = [
+  {
+    title: "Web Development",
+    description: "We have the best web developers.",
+    imgUrl: images.about01,
+  },
+  {
+    title: "React Native Developer",
+    description:
+      "We have the best React Native developers with a passion for building beautiful mobile applications.",
+    imgUrl: images.about02,
+  },
+  {
+    title: "UI/UX",
+    description: "Professionals in User Interface and User Experience.",
+    imgUrl: images.about03,
+  },
+  {
+    title: "Data Engineering",
+    description: "We have the best Data Engineers.",
+    imgUrl: images.about04,
+  },
+];
 
 const About = () => {
-  const [ abouts, setAbouts ] = useState([]);
+  // const [abouts, setAbouts] = useState([]);
 
-  useEffect(() => {
-    const query = '*[_type == "abouts"]';
+  // useEffect(() => {
+  //   const query = '*[_type == "abouts"]';
 
-    client.fetch(query)
-      .then((data) => setAbouts(data))
-  }, []);
-  
+  //   client.fetch(query).then((data) => setAbouts(data));
+  // }, []);
 
   return (
     <>
